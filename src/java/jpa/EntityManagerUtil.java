9d104997124f2e83694480800e1eb29376ef895f
RUI RICHARD BLAESE <RUI RICHARD BLAESE>
2016-10-26 06:23:19 -0200

    enviando projeto

---
+++ src/java/jpa/EntityManagerUtil.java
@@ -0,0 +1,21 @@
+package jpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class EntityManagerUtil {
+
+    private static EntityManagerFactory factory = null;
+    private static EntityManager em = null;
+
+    public static EntityManager getEntityManager() {
+        if (factory == null) {
+            factory = Persistence.createEntityManagerFactory("CasaDasMarmitas");
+        }
+        if (em == null) {
+            em = factory.createEntityManager();
+        }
+        return em;
+    }
+}
